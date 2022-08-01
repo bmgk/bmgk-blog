9c1783fa6b4f4c4c302572e1cfff3b0ba87e37a7
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2022-08-01 22:36:48 +0200

    add mariusz to author page

--- pages/about.js
+++ pages/about.js
@@ -2,20 +2,27 @@ import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
 
 const DEFAULT_LAYOUT = 'AuthorLayout'
+const EMPTY = 'AuthorNoLayout'
 
 export async function getStaticProps() {
   const authorDetails = await getFileBySlug('authors', ['default'])
-  return { props: { authorDetails } }
+  const mariuszDetails = await getFileBySlug('authors', ['mariusz'])
+  return { props: { authorDetails, mariuszDetails } }
 }
 
-export default function About({ authorDetails }) {
-  const { mdxSource, frontMatter } = authorDetails
-
+export default function About({ authorDetails, mariuszDetails }) {
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
+    <>
+      <MDXLayoutRenderer
+        layout={DEFAULT_LAYOUT}
+        mdxSource={authorDetails.mdxSource}
+        frontMatter={authorDetails.frontMatter}
+      />
+      <MDXLayoutRenderer
+        layout={EMPTY}
+        mdxSource={mariuszDetails.mdxSource}
+        frontMatter={mariuszDetails.frontMatter}
+      />
+    </>
   )
 }
